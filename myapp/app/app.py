2030ba128e54a4e99e545a4e9194c0f3cf13d79e
NosiOi <yaroslavfedorak64@gmail.com>
2026-04-14 16:02:27 +0200

    New pages

--- myapp/app/app.py
+++ myapp/app/app.py
@@ -9,5 +9,20 @@ def ingex():
     return render_template("index.html")
 
 
+@app.route("/diet_page")
+def diet():
+    return render_template("diet_page.html")
+
+
+@app.route("/sport")
+def sport():
+    return render_template("sport.html")
+
+
+@app.route("/recovery")
+def recovery():
+    return render_template("recovery.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
