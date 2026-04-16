4ef80ed8ec341e0cbfeb7b1482e799d1ca9447d9
NosiOi <yaroslavfedorak64@gmail.com>
2026-04-16 16:26:24 +0200

    New desing

--- myapp/app/app.py
+++ myapp/app/app.py
@@ -1,12 +1,16 @@
 from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def ingex():
+def index():
     return render_template("index.html")
+
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
 
 
 @app.route("/diet_page")
