cc1170a1ee2b4ec9a70f630336d4b2cbe6f78811
NosiOi <yaroslavfedorak64@gmail.com>
2026-05-07 17:43:54 +0200

    A small sketch of the pages in the folder templates/app was made, and the paths to the pages were also drawn; a problem with the value 'goal' was detected

--- myapp/app/routes/dashboard.py
+++ myapp/app/routes/dashboard.py
@@ -34,6 +34,70 @@ def pricing():
     return render_template("public/pricing.html")
 
 
+@dashboard_bp.route("/sport")
+@login_required
+def sport_page():
+    user = User.query.get(session["user"])
+    return render_template("app/sport.html", user=user)
+
+
+@dashboard_bp.route("/diet_page")
+@login_required
+def diet_page():
+    user = User.query.get(session["user"])
+    return render_template("app/diet_page.html", user=user)
+
+
+@dashboard_bp.route("/recovery")
+@login_required
+def recovery_page():
+    user = User.query.get(session["user"])
+    return render_template("app/recovery.html", user=user)
+
+
+@dashboard_bp.route("/assessment")
+@login_required
+def assessment_page():
+    user = User.query.get(session["user"])
+    return render_template("app/assessment.html", user=user)
+
+
+@dashboard_bp.route("/equipment")
+@login_required
+def equipment_page():
+    user = User.query.get(session["user"])
+    return render_template("app/equipment.html", user=user)
+
+
+@dashboard_bp.route("/training_plan")
+@login_required
+def training_plan_page():
+    user = User.query.get(session["user"])
+    return render_template("app/training_plan.html", user=user)
+
+
+@dashboard_bp.route("/training_explanation")
+@login_required
+def training_explanation_page():
+    user = User.query.get(session["user"])
+    return render_template("app/training_explanation.html", user=user)
+
+
+@dashboard_bp.route("/profile")
+@login_required
+def profile_page():
+    user = User.query.get(session["user"])
+    return render_template("app/profile.html", user=user)
+
+
+@dashboard_bp.route("/questionnaire")
+@login_required
+def questionnaire_page():
+    user = User.query.get(session["user"])
+    return render_template("app/questionnaire.html", user=user)
+
+
+
 @dashboard_bp.route("/plan")
 @login_required
 def plan_page():
