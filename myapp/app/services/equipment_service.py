211fc866e7bc44144d03cf41cb70ddc08dac84c0
NosiOi <yaroslavfedorak64@gmail.com>
2026-06-24 10:10:59 +0200

    Exercise value types have been modified to match the database

--- myapp/app/services/equipment_service.py
+++ myapp/app/services/equipment_service.py
@@ -1,6 +1,6 @@
 from myapp.app import db
 from myapp.app.models.user_equipment import UserEquipment
-from myapp.app.models.equipment import Equipment
+from myapp.app.training_engine.models.equipment import TEEquipment
 
 
 class EquipmentService:
@@ -8,24 +8,38 @@ class EquipmentService:
     @staticmethod
     def get_user_equipment(user):
         items = UserEquipment.query.filter_by(user_id=user.id).all()
-        return [{"id": ue.equipment_id} for ue in items]
+        eq_ids = [ue.equipment_id for ue in items]
+        eq_objects = TEEquipment.query.filter(TEEquipment.id.in_(eq_ids)).all()
+
+        result = []
+        for eq in eq_objects:
+            result.append(eq.to_dict())
+
+        return result
 
     @staticmethod
     def add_equipment(user, equipment_id):
+        eq = TEEquipment.query.filter_by(id=equipment_id).first()
+        if not eq:
+            return False
+
         exists = UserEquipment.query.filter_by(
-            user_id=user.id,
-            equipment_id=equipment_id
+            user_id=user.id, equipment_id=equipment_id
         ).first()
 
-        if not exists:
-            db.session.add(UserEquipment(user_id=user.id, equipment_id=equipment_id))
-            db.session.commit()
+        if exists:
+            return True
+
+        ue = UserEquipment(user_id=user.id, equipment_id=equipment_id, available=True)
+        db.session.add(ue)
+        db.session.commit()
+        return True
 
     @staticmethod
     def remove_equipment(user, equipment_id):
         UserEquipment.query.filter_by(
-            user_id=user.id,
-            equipment_id=equipment_id
+            user_id=user.id, equipment_id=equipment_id
         ).delete()
 
         db.session.commit()
+        return True
