6606385580a631f6244000b7c7a58101ee8979a1
NosiOi <yaroslavfedorak64@gmail.com>
2026-07-06 16:22:08 +0200

    Fix: clean alembic migration and remove wrong auto changes

---
+++ migrations/versions/ea873e3bc158_add_muscles_fields_to_exercises.py
@@ -0,0 +1,22 @@
+"""add muscles fields to exercises
+
+Revision ID: ea873e3bc158
+Revises: 3338bd4cd707
+Create Date: 2026-07-06 16:15:23.095793
+"""
+
+from alembic import op
+import sqlalchemy as sa
+
+revision = "ea873e3bc158"
+down_revision = "3338bd4cd707"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
