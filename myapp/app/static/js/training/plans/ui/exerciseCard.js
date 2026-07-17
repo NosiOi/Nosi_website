e63c86fce9f967d97740675b4cd33c3a1c2e9971
NosiOi <yaroslavfedorak64@gmail.com>
2026-07-17 17:12:48 +0200

    Updated icons and added import

--- myapp/app/static/js/training/plans/ui/exerciseCard.js
+++ myapp/app/static/js/training/plans/ui/exerciseCard.js
@@ -1,5 +1,6 @@
 import { createCounterField, createRepsField } from "./counters.js";
 import { enableDrag } from "../interactions/dragdrop.js";
+import { ICONS } from "../../../icons/icons.js";
 
 export function createExerciseCard(ex, index, list, rerender, openPicker) {
     const card = document.createElement("div");
@@ -12,25 +13,8 @@ export function createExerciseCard(ex, index, list, rerender, openPicker) {
                 <button class="tr-plan-ex-name">${ex.exercise.name}</button>
             </div>
             <div class="tr-plan-card-header-right">
-                <button class="tr-plan-card-drag">
-                    <svg width="18" height="18">
-                        <circle cx="9" cy="12" r="1"/>
-                        <circle cx="9" cy="5" r="1"/>
-                        <circle cx="9" cy="19" r="1"/>
-                        <circle cx="15" cy="12" r="1"/>
-                        <circle cx="15" cy="5" r="1"/>
-                        <circle cx="15" cy="19" r="1"/>
-                    </svg>
-                </button>
-                <button class="tr-plan-card-delete">
-                    <svg width="18" height="18">
-                        <path d="M10 11v6"/>
-                        <path d="M14 11v6"/>
-                        <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6"/>
-                        <path d="M3 6h18"/>
-                        <path d="M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"/>
-                    </svg>
-                </button>
+                <button class="tr-plan-card-drag">${ICONS.grip}</button>
+                <button class="tr-plan-card-delete">${ICONS.delete}</button>
             </div>
         </div>
         <div class="tr-plan-card-body"></div>
