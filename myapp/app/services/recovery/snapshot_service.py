1dad8561d14aaf9bd4558a34ae3ca7c7429c993a
NosiOi <yaroslavfedorak64@gmail.com>
2026-07-20 18:46:50 +0200

    Add snapshot service as central recovery aggregator

--- myapp/app/services/recovery/snapshot_service.py
+++ myapp/app/services/recovery/snapshot_service.py
@@ -5,21 +5,24 @@ from myapp.app.services.recovery.recovery_score_service import RecoveryScoreServ
 
 
 class SnapshotService:
-    def generate_snapshot(self, user_id, last_training_days):
-        scores = RecoveryScoreService()
+    def __init__(self):
+        self.scores = RecoveryScoreService()
 
-        sleep_score = scores.calculate_sleep_score(user_id)
-        habit_score = scores.calculate_habit_score(user_id)
-        training_score = scores.calculate_training_score(last_training_days)
-        energy_score = scores.calculate_energy_score(
+    def generate_snapshot(self, user_id, last_training_days):
+        today = date.today()
+
+        sleep_score = self.scores.calculate_sleep_score(user_id)
+        habit_score = self.scores.calculate_habit_score(user_id)
+        training_score = self.scores.calculate_training_score(last_training_days)
+        energy_score = self.scores.calculate_energy_score(
             sleep_score, habit_score, training_score
         )
-        recovery_score = scores.calculate_recovery_score(
+        recovery_score = self.scores.calculate_recovery_score(
             sleep_score, habit_score, training_score, energy_score
         )
 
         snapshot = DailyRecoverySnapshot.query.filter_by(
-            user_id=user_id, date=date.today()
+            user_id=user_id, date=today
         ).first()
         if snapshot:
             snapshot.sleep_score = sleep_score
@@ -30,7 +33,7 @@ class SnapshotService:
         else:
             snapshot = DailyRecoverySnapshot(
                 user_id=user_id,
-                date=date.today(),
+                date=today,
                 sleep_score=sleep_score,
                 habit_score=habit_score,
                 training_score=training_score,
