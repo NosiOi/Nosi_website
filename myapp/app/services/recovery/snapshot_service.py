1dcdae8b1b0176cfd058f76fa976ad4cb8a04e20
NosiOi <yaroslavfedorak64@gmail.com>
2026-07-24 18:01:33 +0200

    Extend SnapshotService to include full sleep data in daily snapshot generation

--- myapp/app/services/recovery/snapshot_service.py
+++ myapp/app/services/recovery/snapshot_service.py
@@ -2,22 +2,28 @@ from datetime import date
 from myapp.app import db
 from myapp.app.models.recovery.daily_recovery_snapshot import DailyRecoverySnapshot
 from myapp.app.services.recovery.recovery_score_service import RecoveryScoreService
+from myapp.app.services.recovery.sleep_service import SleepService
 
 
 class SnapshotService:
     def __init__(self):
         self.scores = RecoveryScoreService()
+        self.sleep_service = SleepService()
 
     def update_snapshot(
         self,
         snapshot,
-        sleep_score,
+        sleep_data,
         habit_score,
         training_score,
         energy_score,
         recovery_score,
     ):
-        snapshot.sleep_score = sleep_score
+        snapshot.sleep_score = sleep_data["sleep_score"]
+        snapshot.sleep_duration_minutes = sleep_data["duration"]
+        snapshot.sleep_start = sleep_data["start"]
+        snapshot.sleep_end = sleep_data["end"]
+
         snapshot.habit_score = habit_score
         snapshot.training_score = training_score
         snapshot.energy_score = energy_score
@@ -26,14 +32,16 @@ class SnapshotService:
     def generate_snapshot(self, user_id, last_training_days):
         today = date.today()
 
-        sleep_score = self.scores.calculate_sleep_score(user_id)
+        sleep_entry = self.sleep_service.get_last_sleep(user_id)
+        sleep_data = self.sleep_service.get_sleep_data(sleep_entry)
+
         habit_score = self.scores.calculate_habit_score(user_id)
         training_score = self.scores.calculate_training_score(last_training_days)
         energy_score = self.scores.calculate_energy_score(
-            sleep_score, habit_score, training_score
+            sleep_data["sleep_score"], habit_score, training_score
         )
         recovery_score = self.scores.calculate_recovery_score(
-            sleep_score, habit_score, training_score, energy_score
+            sleep_data["sleep_score"], habit_score, training_score, energy_score
         )
 
         snapshot = DailyRecoverySnapshot.query.filter_by(
@@ -43,7 +51,7 @@ class SnapshotService:
         if snapshot:
             self.update_snapshot(
                 snapshot,
-                sleep_score,
+                sleep_data,
                 habit_score,
                 training_score,
                 energy_score,
@@ -53,7 +61,10 @@ class SnapshotService:
             snapshot = DailyRecoverySnapshot(
                 user_id=user_id,
                 date=today,
-                sleep_score=sleep_score,
+                sleep_score=sleep_data["sleep_score"],
+                sleep_duration_minutes=sleep_data["duration"],
+                sleep_start=sleep_data["start"],
+                sleep_end=sleep_data["end"],
                 habit_score=habit_score,
                 training_score=training_score,
                 energy_score=energy_score,
