683cac0a3accf75d45aac3450332bafe5d385479
NosiOi <yaroslavfedorak64@gmail.com>
2026-07-26 10:32:42 +0200

    Simplify snapshot generation using refined recovery score service

--- myapp/app/services/recovery/snapshot_service.py
+++ myapp/app/services/recovery/snapshot_service.py
@@ -1,5 +1,4 @@
 from datetime import date
-
 from myapp.app import db
 from myapp.app.models.recovery.daily_recovery_snapshot import DailyRecoverySnapshot
 from myapp.app.services.recovery.recovery_score_service import RecoveryScoreService
@@ -24,6 +23,7 @@ class SnapshotService:
         snapshot.sleep_duration_minutes = sleep_data["duration"]
         snapshot.sleep_start = sleep_data["start"]
         snapshot.sleep_end = sleep_data["end"]
+
         snapshot.habit_score = habit_score
         snapshot.training_score = training_score
         snapshot.energy_score = energy_score
@@ -38,13 +38,21 @@ class SnapshotService:
         sleep_score = sleep_data["sleep_score"]
         habit_score = self.scores.calculate_habit_score(user_id)
         training_score = self.scores.calculate_training_score(user_id)
-        energy_score = self.scores.calculate_energy_score(sleep_score, habit_score)
+        energy_score = self.scores.calculate_energy_score(
+            sleep_score,
+            habit_score,
+        )
         recovery_score = self.scores.calculate_recovery_score(
-            user_id, sleep_score, habit_score, training_score
+            user_id,
+            sleep_score,
+            habit_score,
+            training_score,
+            energy_score,
         )
 
         snapshot = DailyRecoverySnapshot.query.filter_by(
-            user_id=user_id, date=today
+            user_id=user_id,
+            date=today,
         ).first()
 
         if snapshot:
@@ -60,7 +68,7 @@ class SnapshotService:
             snapshot = DailyRecoverySnapshot(
                 user_id=user_id,
                 date=today,
-                sleep_score=sleep_score,
+                sleep_score=sleep_data["sleep_score"],
                 sleep_duration_minutes=sleep_data["duration"],
                 sleep_start=sleep_data["start"],
                 sleep_end=sleep_data["end"],
