3278eba870b0bff66491f24aa5ba6b4e08aa2fe6
NosiOi <yaroslavfedorak64@gmail.com>
2026-07-27 17:22:03 +0200

    Make sets_planned, reps_planned, load_planned nullable in session_exercises

---
+++ migrations/versions/9aa43c34992e_make_planned_columns_nullable_in_.py
@@ -0,0 +1,24 @@
+"""make planned columns nullable in session_exercises
+
+Revision ID: 9aa43c34992e
+Revises: 9302fb241b91
+Create Date: 2026-07-27 17:21:27.853165
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '9aa43c34992e'
+down_revision = '9302fb241b91'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
