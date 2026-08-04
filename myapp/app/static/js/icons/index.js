4ac594171b3ab51e8b479ac269f445eec43115ab
NosiOi <yaroslavfedorak64@gmail.com>
2026-08-04 16:43:52 +0200

    Refactor icon system: modular icons/, add nutrition.js, tracker.js, profile.js, unify ICONS entrypoint

--- myapp/app/static/js/icons/index.js
+++ myapp/app/static/js/icons/index.js
@@ -1,11 +1,17 @@
 import { COMMON_ICONS } from "./common.js";
-import { RECOVERY_ICONS } from "./recovery.js";
 import { TRAINING_ICONS } from "./training.js";
+import { RECOVERY_ICONS } from "./recovery.js";
 import { HABIT_ICONS } from "./habit_icons.js";
+import { NUTRITION_ICONS } from "./nutrition.js";
+import { TRACKER_ICONS } from "./tracker.js";
+import { PROFILE_ICONS } from "./profile.js";
 
 export const ICONS = {
     ...COMMON_ICONS,
-    ...RECOVERY_ICONS,
     ...TRAINING_ICONS,
-    ...HABIT_ICONS
+    ...RECOVERY_ICONS,
+    ...HABIT_ICONS,
+    ...NUTRITION_ICONS,
+    ...TRACKER_ICONS,
+    ...PROFILE_ICONS
 };
